aced450c426a4cf6392422eef995d8096fa69690
Rebeca Luchini <rebeca.luchini20@gmail.com>
2025-08-20 19:57:30 -0300

    Update RevisaoApplication.java

--- To-Do-List-main/backend/src/main/java/com/rev/revisao/RevisaoApplication.java
+++ To-Do-List-main/backend/src/main/java/com/rev/revisao/RevisaoApplication.java
@@ -3,11 +3,12 @@ package com.rev.revisao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // classe principal da aplicação springboot (configura o spring, configura tudo automaticamente, encontra seus componentes no projeto)
 public class RevisaoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RevisaoApplication.class, args);
+	public static void main(String[] args) { // metodo principal publico e estático que inicia a aplicação, recebe argumentos e nn retorna nada
+
+		SpringApplication.run(RevisaoApplication.class, args); // O metodo run cria o contexto do spring, configura os componentes e inicia o servidor
 	}
 
 }
