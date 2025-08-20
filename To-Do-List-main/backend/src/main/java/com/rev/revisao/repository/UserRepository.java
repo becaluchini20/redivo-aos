a4bf15649b1476706c5b4f2443c37ceeef374e27
Rebeca Luchini <rebeca.luchini20@gmail.com>
2025-08-20 19:42:48 -0300

    Update UserRepository.java

--- To-Do-List-main/backend/src/main/java/com/rev/revisao/repository/UserRepository.java
+++ To-Do-List-main/backend/src/main/java/com/rev/revisao/repository/UserRepository.java
@@ -1,3 +1,5 @@
+//essa pasta guarda as interfaces que fazem a comunicação com o bd
+
 package com.rev.revisao.repository;
 
 import com.rev.revisao.model.User;
@@ -6,3 +8,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> { //está criando uma interface chamada userrepository e dizendo que ela herda de jparepository
     boolean existsByEmail(String email); //cria uma consulta que verifica se existe um usuario com o email informado
 }
+//o jpa é uma interface que facilita acessar e manipular dados no bd de forma simples
